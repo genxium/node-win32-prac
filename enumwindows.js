4318ac7be708ef244d6a599b357237d3bb9b3a2f
Wingfung Lo <Wingfung Lo>
2017-04-20 01:18:28 +0800

    Fixes for referencing & dereferencing.

--- enumwindows.js
+++ enumwindows.js
@@ -9,29 +9,31 @@ const voidPtr = ref.refType(voidType);
 const stringPtr = ref.refType(ref.types.CString);
 const int32Type = ref.types.int32;
 const boolType = ref.types.bool;
-const longType = ref.types.long;
-const dwordType = longType;
 
+const longType = ref.types.long;
 const longPtr = ref.refType(longType);
 const hwndType = longType;
-const lparamType = longPtr;
+const lparamType = longPtr; 
+
+const ulongType = ref.types.ulong;
+const dwordType = ulongType;
+const lpdwordType = ref.refType(dwordType);
 
 const user32 = ffi.Library('user32.dll', {
   EnumWindows: [boolType, [voidPtr, int32Type]],
   GetWindowTextA: [dwordType, [hwndType, stringPtr, longType]],
-  GetWindowThreadProcessId: [dwordType, [hwndType, lparamType]],  
+  GetWindowThreadProcessId: [dwordType, [hwndType, lpdwordType]],  
   IsWindowVisible: [boolType, [hwndType]],
 });
 
-const windowProc = ffi.Callback('bool', ['long', 'int32'], function(hwnd, lParam) {
-  let buf1 = new Buffer(255);
+const windowProc = ffi.Callback(boolType, [hwndType, lparamType], function(hwnd, lParam) {
+  const buf1 = new Buffer(255);
   const ret1 = user32.GetWindowTextA(hwnd, buf1, 255);
   const name = ref.readCString(buf1, 0);
 
-  let buf2 = new Buffer(255);
-  const ret2 = user32.GetWindowThreadProcessId(hwnd, buf2);
-  
-  const pid = ref.readInt64(buf2, 0);
+  const buf2 = ref.alloc(dwordType);
+  const ret2 = user32.GetWindowThreadProcessId(hwnd, buf2); 
+  const pid = buf2.deref();
 
   const isOnScreen = user32.IsWindowVisible(hwnd);
   if (!isOnScreen) return true;
