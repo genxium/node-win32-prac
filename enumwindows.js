f46909b4d8b08f8209f3ca18838cdaaca9106057
Wingfung Lo <Wingfung Lo>
2017-04-20 00:44:13 +0800

    Updated `enumwindows`.

--- enumwindows.js
+++ enumwindows.js
@@ -4,19 +4,39 @@
 const ref = require('ref');
 const ffi = require('ffi');
 
-const voidPtr = ref.refType(ref.types.void);
+const voidType = ref.types.void;
+const voidPtr = ref.refType(voidType);
 const stringPtr = ref.refType(ref.types.CString);
+const int32Type = ref.types.int32;
+const boolType = ref.types.bool;
+const longType = ref.types.long;
+const dwordType = longType;
+
+const longPtr = ref.refType(longType);
+const hwndType = longType;
+const lparamType = longPtr;
 
 const user32 = ffi.Library('user32.dll', {
-  EnumWindows: ['bool', [voidPtr, 'int32']],
-  GetWindowTextA : ['long', ['long', stringPtr, 'long']]
+  EnumWindows: [boolType, [voidPtr, int32Type]],
+  GetWindowTextA: [dwordType, [hwndType, stringPtr, longType]],
+  GetWindowThreadProcessId: [dwordType, [hwndType, lparamType]],  
+  IsWindowVisible: [boolType, [hwndType]],
 });
 
 const windowProc = ffi.Callback('bool', ['long', 'int32'], function(hwnd, lParam) {
-  let buf = new Buffer(255);
-  const ret = user32.GetWindowTextA(hwnd, buf, 255);
-  const name = ref.readCString(buf, 0);
-  console.log(name);
+  let buf1 = new Buffer(255);
+  const ret1 = user32.GetWindowTextA(hwnd, buf1, 255);
+  const name = ref.readCString(buf1, 0);
+
+  let buf2 = new Buffer(255);
+  const ret2 = user32.GetWindowThreadProcessId(hwnd, buf2);
+  
+  const pid = ref.readInt64(buf2, 0);
+
+  const isOnScreen = user32.IsWindowVisible(hwnd);
+  if (!isOnScreen) return true;
+
+  console.log('On screen window \"' + name + '\" belongs to pid ' + pid);
   return true;
 });
 
