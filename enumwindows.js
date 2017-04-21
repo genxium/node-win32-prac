f580b06c65dda7958138d18722f7deabd8fe56e7
Wingfung Lo <Wingfung Lo>
2017-04-21 10:59:55 +0800

    Minor update.

--- enumwindows.js
+++ enumwindows.js
@@ -6,7 +6,13 @@ const ffi = require('ffi');
 
 const voidType = ref.types.void;
 const voidPtr = ref.refType(voidType);
-const stringPtr = ref.refType(ref.types.CString);
+
+const stringType = ref.types.CString;
+const stringPtr = ref.refType(stringType);
+
+const intType = ref.types.int;
+const uintType = ref.types.uint;
+
 const int32Type = ref.types.int32;
 const boolType = ref.types.bool;
 
@@ -21,6 +27,7 @@ const lpdwordType = ref.refType(dwordType);
 
 const user32 = ffi.Library('user32.dll', {
   EnumWindows: [boolType, [voidPtr, int32Type]],
+  GetClassNameA: [dwordType, [hwndType, stringPtr, longType]],
   GetWindowTextA: [dwordType, [hwndType, stringPtr, longType]],
   GetWindowThreadProcessId: [dwordType, [hwndType, lpdwordType]],  
   IsWindowVisible: [boolType, [hwndType]],
@@ -31,14 +38,18 @@ const windowProc = ffi.Callback(boolType, [hwndType, lparamType], function(hwnd,
   const ret1 = user32.GetWindowTextA(hwnd, buf1, 255);
   const name = ref.readCString(buf1, 0);
 
+  const isOnScreen = user32.IsWindowVisible(hwnd);
+  if (!isOnScreen) return true;
+
   const buf2 = ref.alloc(dwordType);
   const ret2 = user32.GetWindowThreadProcessId(hwnd, buf2); 
   const pid = buf2.deref();
 
-  const isOnScreen = user32.IsWindowVisible(hwnd);
-  if (!isOnScreen) return true;
+  const buf3 = new Buffer(512); 
+  const ret3 = user32.GetClassNameA(hwnd, buf3, 512); 
+  const className = ref.readCString(buf3, 0); 
 
-  console.log('On screen window \"' + name + '\" belongs to pid ' + pid);
+  console.log('On screen window \"' + name + ':' + className + '\" belongs to pid ' + pid);
   return true;
 });
 
