49b1b14604e2c63749c91129424995b8587e6f2d
Wingfung Lo <Wingfung Lo>
2017-04-22 22:28:09 +0800

    Added `send_F5` script.

--- send_F5.js
+++ send_F5.js
@@ -11,7 +11,10 @@ const stringType = ref.types.CString;
 const stringPtr = ref.refType(stringType);
 
 const intType = ref.types.int;
+const intPtr = ref.refType(intType);
+
 const uintType = ref.types.uint;
+const wparamType = uintType;
 
 const int32Type = ref.types.int32;
 const boolType = ref.types.bool;
@@ -25,32 +28,52 @@ const ulongType = ref.types.ulong;
 const dwordType = ulongType;
 const lpdwordType = ref.refType(dwordType);
 
+const targetPidStr = process.env.PID;
+
+let targetWindowList = [];
+
 const user32 = ffi.Library('user32.dll', {
   EnumWindows: [boolType, [voidPtr, int32Type]],
-  GetClassNameA: [dwordType, [hwndType, stringPtr, longType]],
   GetWindowTextA: [dwordType, [hwndType, stringPtr, longType]],
+  GetClassNameA: [dwordType, [hwndType, stringPtr, longType]],
   GetWindowThreadProcessId: [dwordType, [hwndType, lpdwordType]],  
-  IsWindowVisible: [boolType, [hwndType]],
+  PostMessageA: [boolType, [hwndType, uintType, wparamType, lparamType]],
 });
 
 const windowProc = ffi.Callback(boolType, [hwndType, lparamType], function(hwnd, lParam) {
-  const buf1 = new Buffer(255);
-  const ret1 = user32.GetWindowTextA(hwnd, buf1, 255);
-  const name = ref.readCString(buf1, 0);
-
-  const isOnScreen = user32.IsWindowVisible(hwnd);
-  if (!isOnScreen) return true;
-
   const buf2 = ref.alloc(dwordType);
   const ret2 = user32.GetWindowThreadProcessId(hwnd, buf2); 
   const pid = buf2.deref();
 
+  if (pid.toString() !== targetPidStr) return true;
+
+  const buf1 = new Buffer(1024);
+  const ret1 = user32.GetWindowTextA(hwnd, buf1, 1024);
+  const name = ref.readCString(buf1, 0);
+
   const buf3 = new Buffer(512); 
   const ret3 = user32.GetClassNameA(hwnd, buf3, 512); 
   const className = ref.readCString(buf3, 0); 
+ 
+  targetWindowList.push({
+    windowId: hwnd, 
+    windowName: name,
+    windowClassName: className,
+  });
 
-  console.log('On screen window \"' + name + ':' + className + '\" belongs to pid ' + pid);
   return true;
 });
 
 user32.EnumWindows(windowProc, 0);
+
+const WM_KEYDOWN = 0x0100;
+const WM_KEYUP = 0x0101;
+const VK_F5 = 0x74;
+const VK_F6 = 0x75;
+
+for (let i = 0; i < targetWindowList.length; ++i) {
+  const target = targetWindowList[i];
+  console.log('Found target window ' + target.windowName + ':' + target.windowClassName);
+  user32.PostMessageA(target.windowId, WM_KEYDOWN, VK_F5, 0);
+  user32.PostMessageA(target.windowId, WM_KEYUP, VK_F5, 0);
+}
